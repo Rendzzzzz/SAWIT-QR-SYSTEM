acf8fd553adec845e4d360416754cec5936388e5
Rendzzzz <rendhihalimdp@gmail.com>
2026-06-17 03:58:58 +0700

    Reset cache and config

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,48 @@
+<?php return array (
+  'bref/laravel-bridge' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Bref\\LaravelBridge\\BrefServiceProvider',
+    ),
+  ),
+  'bref/laravel-health-check' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Bref\\LaravelHealthCheck\\BrefHealthCheckServiceProvider',
+    ),
+  ),
+  'laravel/octane' => 
+  array (
+    'aliases' => 
+    array (
+      'Octane' => 'Laravel\\Octane\\Facades\\Octane',
+    ),
+    'providers' => 
+    array (
+      0 => 'Laravel\\Octane\\OctaneServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+);
