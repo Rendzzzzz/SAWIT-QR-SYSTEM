7d00e1cd35cd38afd06322e404c6a6b9ec99f8b3
Rendzzzz <rendhihalimdp@gmail.com>
2026-06-17 02:38:00 +0700

    Remove Railway specific files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,29 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\App;
-
-public function boot()
-{
-    // Jika di lingkungan production Railway, skip migrations
-    if (App::environment('production')) {
-        $this->loadMigrationsFrom([]);
-    }
-}
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         //
