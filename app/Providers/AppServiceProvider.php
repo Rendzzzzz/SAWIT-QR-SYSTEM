4fdff7aba0350c256087b9a3c7a01924dd3469cf
Rendzzzzz <rendhihd@gmail.com>
2026-06-17 06:36:25 +0700

    Refactor boot method in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,14 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Blade;
-
-public function boot()
-{
-    // Nonaktifkan view cache
-    config(['view.compiled' => null]);
-}
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +13,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        //
+        // Nonaktifkan view cache
+        config(['view.compiled' => null]);
     }
 }
